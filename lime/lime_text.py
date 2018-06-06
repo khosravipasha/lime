6795eaf24635b4cb8accb911e7ed686c96c322d3
Pasha Khosravi <khosravipasha@gmail.com>
2018-06-05 20:47:58 -0700

    added functions used for blocks

--- lime/lime_text.py
+++ lime/lime_text.py
@@ -11,8 +11,8 @@ import numpy as np
 import scipy as sp
 import sklearn
 
-from . import explanation
-from . import lime_base
+import explanation
+import lime_base
 
 
 class TextDomainMapper(explanation.DomainMapper):
@@ -169,6 +169,17 @@ class IndexedString(object):
             return self.positions[words]
 
 
+    def get_sentence(self, bitmask):
+        '''
+        bitmask: vector of 0 and 1's on what worsd to choose
+        '''
+        words = []
+        for i in range(len(bitmask)):
+            if bitmask[i] == 1:
+                words.append(self.as_list[self.positions[i]])
+
+        return " ".join(words)
+
 class LimeTextExplainer(object):
     """Explains text classifiers.
        Currently, we are using an exponential kernel on cosine distance, and
@@ -273,7 +284,6 @@ class LimeTextExplainer(object):
                 feature_selection=self.feature_selection)
         return ret_exp
 
-    @classmethod
     def __data_labels_distances(cls,
                                 indexed_string,
                                 classifier_fn,
@@ -320,6 +330,130 @@ class LimeTextExplainer(object):
             inactive = np.random.choice(features_range, size, replace=False)
             data[i, inactive] = 0
             inverse_data.append(indexed_string.inverse_removing(inactive))
+        labels = classifier_fn(inverse_data)
+        distances = distance_fn(sp.sparse.csr_matrix(data))
+        return data, labels, distances
+
+    def explain_json_instance(self,
+                         text_instance,
+                         classifier_fn,
+                         data,
+                         labels=(1,),
+                         top_labels=None,
+                         num_features=10,
+                         distance_metric='cosine',
+                         model_regressor=None):
+        """Generates explanations for a prediction.
+
+        First, we generate neighborhood data by randomly hiding features from
+        the instance (see __data_labels_distance_mapping). We then learn
+        locally weighted linear models on this neighborhood data to explain
+        each of the classes in an interpretable way (see lime_base.py).
+
+        Args:
+            text_instance: raw text string to be explained.
+            classifier_fn: classifier prediction probability function, which
+                takes a list of d strings and outputs a (d, k) numpy array with
+                prediction probabilities, where k is the number of classes.
+                For ScikitClassifiers , this is classifier.predict_proba.
+            labels: iterable with labels to be explained.
+            top_labels: if not None, ignore labels and produce explanations for
+                the K labels with highest prediction probabilities, where K is
+                this parameter.
+            num_features: maximum number of features present in explanation
+            data: dense num_samples * K binary matrix, where K is the
+                number of tokens in indexed_string. The first row is the
+                original instance, and thus a row of ones.
+            distance_metric: the distance metric to use for sample weighting,
+                defaults to cosine similarity
+            model_regressor: sklearn regressor to use in explanation. Defaults
+            to Ridge regression in LimeBase. Must have model_regressor.coef_
+            and 'sample_weight' as a parameter to model_regressor.fit()
+        Returns:
+            An Explanation object (see explanation.py) with the corresponding
+            explanations.
+        """
+        num_samples = len(data)
+        indexed_string = IndexedString(text_instance, bow=False,
+                                       split_expression=self.split_expression)
+        domain_mapper = TextDomainMapper(indexed_string)
+
+        data, yss, distances = self.__data_labels_distances_from_json(
+            indexed_string, classifier_fn, data,
+            distance_metric=distance_metric)
+
+
+        if self.class_names is None:
+            self.class_names = [str(x) for x in range(yss[0].shape[0])]
+        ret_exp = explanation.Explanation(domain_mapper=domain_mapper,
+                                          class_names=self.class_names)
+        ret_exp.predict_proba = yss[0]
+        if top_labels:
+            labels = np.argsort(yss[0])[-top_labels:]
+            ret_exp.top_labels = list(labels)
+            ret_exp.top_labels.reverse()
+        for label in labels:
+            (ret_exp.intercept[label],
+             ret_exp.local_exp[label],
+             ret_exp.score) = self.base.explain_instance_with_data(
+                data, yss, distances, label, num_features,
+                model_regressor=model_regressor,
+                feature_selection=self.feature_selection)
+        return ret_exp
+
+
+    def __data_labels_distances_from_json(cls,
+                                indexed_string,
+                                classifier_fn,
+                                data,
+                                distance_metric='cosine'):
+        """Generates a neighborhood around a prediction.
+
+        Generates neighborhood data by randomly removing words from
+        the instance, and predicting with the classifier. Uses cosine distance
+        to compute distances between original and perturbed instances.
+        Args:
+            indexed_string: document (IndexedString) to be explained,
+            classifier_fn: classifier prediction probability function, which
+                takes a string and outputs prediction probabilities. For
+                ScikitClassifier, this is classifier.predict_proba.
+            data: dense num_samples * K binary matrix, where K is the
+                number of tokens in indexed_string. The first row is the
+                original instance, and thus a row of ones.
+            distance_metric: the distance metric to use for sample weighting,
+                defaults to cosine similarity
+
+
+        Returns:
+            A tuple (data, labels, distances), where:
+                data: dense num_samples * K binary matrix, where K is the
+                    number of tokens in indexed_string. The first row is the
+                    original instance, and thus a row of ones.
+                labels: num_samples * L matrix, where L is the number of target
+                    labels
+                distances: cosine distance between the original instance and
+                    each perturbed instance (computed in the binary 'data'
+                    matrix), times 100.
+        """
+
+        def distance_fn(x):
+            return sklearn.metrics.pairwise.pairwise_distances(
+                x, x[0], metric=distance_metric).ravel() * 100
+
+        num_samples = len(data)
+        doc_size = indexed_string.num_words()
+        #sample = np.random.randint(1, doc_size + 1, num_samples - 1)
+        #data = np.ones((num_samples, doc_size))
+        #data[0] = np.ones(doc_size)
+        features_range = range(doc_size)
+        inverse_data = []
+        for i in range(num_samples):
+            inverse_data.append(indexed_string.get_sentence(data[i]))
+
+        #for i, size in enumerate(sample, start=1):
+            #inactive = np.random.choice(features_range, size, replace=False)
+            #data[i, inactive] = 0
+            #inverse_data.append(indexed_string.inverse_removing(inactive))
         labels = classifier_fn(inverse_data)
         distances = distance_fn(sp.sparse.csr_matrix(data))
         return data, labels, distances
